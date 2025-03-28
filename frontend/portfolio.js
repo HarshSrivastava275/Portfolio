65ecda9dab20f3c7a7a966836c1d995474d58c66
HarshSrivastava275 <harshsri987654@gmail.com>
2025-03-28 18:06:56 +0530

    Update portfolio.js

--- frontend/portfolio.js
+++ frontend/portfolio.js
@@ -39,7 +39,7 @@ document.getElementById("contactForm").addEventListener("submit", async function
 
     try {
         
-        const response = await fetch("https://portfolio-vr3x.onrender.com", { 
+        const response = await fetch("/contact", { 
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ name, email, message })
